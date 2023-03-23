58881aeb74cad6a6b295753eed5f1ffe14d0b0fb
Logan Philbrick <loganphilbrick99@gmail.com>
2023-03-23 13:48:52 -0400

    Navigation Part 2

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -3,17 +3,29 @@ import Constants from "expo-constants";
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import DirectoryScreen from "./DirectoryScreen";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./HomeScreen";
 
-const Directorynavigator = () => {
+const Drawer = createDrawerNavigator();
+
+const screenOptions = {
+  headerTintColor: "#fff",
+  headerStyle: { backgroundColor: "#5637DD" },
+};
+
+const HomeNavigator = () => {
   const Stack = createStackNavigator();
   return (
-    <Stack.Navigator
-      initialRouteName="Directory"
-      screenOptions={{
-        headerStyle: { backgroundColor: "#5637DD" },
-        headerTintColor: "#FFF",
-      }}
-    >
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Home" }} />
+    </Stack.Navigator>
+  );
+};
+
+const DirectoryNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator initialRouteName="Directory" screenOptions={screenOptions}>
       <Stack.Screen name="Directory" component={DirectoryScreen} options={{ title: "Campsite Directory" }} />
       <Stack.Screen
         name="CampsiteInfo"
@@ -29,7 +41,10 @@ const Directorynavigator = () => {
 const Main = () => {
   return (
     <View style={{ flex: 1, paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight }}>
-      <Directorynavigator />
+      <Drawer.Navigator initialRouteName="Home" drawerStyle={{ backgroundColor: "#CEC8FF" }}>
+        <Drawer.Screen name="Home" component={HomeNavigator} options={{ title: "Home" }} />
+        <Drawer.Screen name="Directory" component={DirectoryNavigator} options={{ title: "Directory" }} />
+      </Drawer.Navigator>
     </View>
   );
 };
